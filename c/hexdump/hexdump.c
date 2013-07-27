b3038220b3628e8ad2a6ec0e5182aa930f896076
鎌田 大貴 <daiki@kamata-no-MacBook-Air.local>
2013-07-27 17:40:46 +0900

    debug ver

--- c/hexdump/hexdump.c
+++ c/hexdump/hexdump.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
@@ -7,7 +8,7 @@ int main(int argc, char *argv[]){
     char ch;
     int read_bytecount = 0;
     int hexchar[16];
-    char read_filein[17];
+    char read_char[17];
     char *filename = argv[1];
     FILE *fp;
     
@@ -16,64 +17,99 @@ int main(int argc, char *argv[]){
         printf("error : can\'t open %s",filename);
         exit(1);
     }
+            for(ix = 0; ix < 16; ix++)
+            {
+                read_char[ix] = '\0';
+            }
 
     /*-- comment --*/
     while ((ch = fgetc(fp)) != EOF)
     {
         read_bytecount += 1;
-        hexchar[(read_bytecount - 1) % 16] = ch;
-        read_filein[(read_bytecount - 1) % 16] = ch;
+        read_char[((read_bytecount-1) % 16)] = ch;
+//        printf("%d %c %zd \n",(read_bytecount-1) % 16,ch , strlen(read_char));
 
-        if(read_bytecount % 16 == 0)
+//        if( (int)strlen(read_char) == 16 )//文字列満タンなら
+        if( read_bytecount % 16  == 0 )
         {
+
             /*-- print read byte count --*/
             printf("%08x  ",read_bytecount - 16);
             /*-- print hexdump --*/
             for(ix = 0; ix < 16; ix++)
             {
-                printf("%02x ",hexchar[ix]);
+                printf("%02x ",read_char[ix]);
             }
             /*-- print filein --*/
-            printf(" | ");
+            printf(" |");
             for(ix = 0; ix < 16; ix++)
             {
                 //printf("%d:",ix);
-                if(read_filein[ix] == '\n')
+                if(read_char[ix] == '\n')
                 {
                     printf(".");
                 }else{
-                    printf("%c",read_filein[ix]);
+                    printf("%c",read_char[ix]);
                 }
             }
-            printf(" |\n");
+//            for(ix = 0; ix < 17; ix++)
+//            {
+//                read_char[ix] = '\0';
+//            }
+            printf("|\n");
         }
     }
-        printf("%08x  ",read_bytecount - 16);
-        for(ix = 0; ix < ((read_bytecount - 1) % 16); ix++)
-        {
-            printf("%02x ",hexchar[ix]);
-        }
-        for(ix = ((read_bytecount - 1) % 16); ix < 16; ix++)
-        {
-            printf("   ");
-        }
-        printf(" | ");
-        for(ix = 0; ix < ((read_bytecount - 1) % 16); ix++)
-        {
-            //printf("%d:",ix);
-            if(read_filein[ix] == '\n')
-            {
-                printf(".");
-            }else{
-                printf("%c",read_filein[ix]);
-            }
-        }
-        printf(" |\n");
+//        printf("%08x  ",read_bytecount);
+//        for(ix = 0; ix < ((read_bytecount - 1) % 16); ix++)
+//        {
+//            printf("%02x ",hexchar[ix]);
+//        }
+//        for(ix = ((read_bytecount - 1) % 16); ix < 16; ix++)
+//        {
+//            printf("   ");
+//        }
+//        printf(" |");
+//        for(ix = 0; ix < ((read_bytecount - 1) % 16); ix++)
+//        {
+//            //printf("%d:",ix);
+//            if(read_filein[ix] == '\n')
+//            {
+//                printf(".");
+//            }else{
+//                printf("%c",read_filein[ix]);
+//            }
+//        }
+//        printf("|\n");
 
     fclose(fp);
 
     return 0;
 }
 
-void print_oneline(int rbc_p, int hch_p, int rfi_p){
-}
+//int print_oneline(int *read_bytecount, int *hexchar, int *read_filein)
+//{
+//    int ix;
+//    printf("%08x  ",*read_bytecount-16);
+////    for(ix = 0; ix < ((*read_bytecount - 1) % 16); ix++)
+//    for(ix = 0; ix <  16; ix++)
+//    {
+//        printf("%02x ",hexchar[ix]);
+//    }
+////    for(ix = (*read_bytecount % 16); ix < 16; ix++)
+////    {
+////        printf("   ");
+////    }
+//    printf(" |");
+////    for(ix = 0; ix < ((*read_bytecount - 1) % 16); ix++)
+//    for(ix = 0; ix < 16; ix++)
+//    {
+//        printf("%d:",ix);
+//        if(read_filein[ix] == '\n')
+//        {
+//            printf(".");
+//        }else{
+//            printf("%c",read_filein[ix]);
+//        }
+//    }
+//    printf("|\n");
+//}
